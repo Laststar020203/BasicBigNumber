27f7d2ee25de333cff41920571326b8179be314f
FunCorder <44064404+Laststar020203@users.noreply.github.com>
2019-04-04 16:34:42 +0900

    2019-04-04

--- src/CBigIntegerOP.java
+++ src/CBigIntegerOP.java
@@ -6,21 +6,40 @@
  * 아직 더하기 밖에 못만듬 추후에 뺄셈 추가 예정
  *
  */
-public class CBigInteger {
+public class CBigIntegerOP {
 
 	private static long[] one;
 	private static long[] two;
+	public static long[] value;
+	private static final long compareDigit = 1000000000000000000L;
 
-	private CBigInteger() {
+	private CBigIntegerOP() {
 
 	}
+	
+	/**
+	 * 
+	 * @param number 피연산자
+	 * @param square 피연산자의 몇승을 구할건지?
+	 * @return
+	 */
+	public static String Square(int number , int square) {
+		String value = number+"";
+		
+		for (int i = 1; i < square; i++) {
+			value = add(value, value);
+		}	
+		
+		return value;
+	}
+	
 
 	/**
 	 * 
 	 * 피연산자를 각 자리별대로 저장한 long타입 배열 one, two를 같은 인덱스 끼리 더한다음 나온 값을 문자열로 다시 변환해 작은
 	 * 자리수부터 큰 자리수 방향으로 차례대로 붙입니다. 예시) 한자리마다 배열에 들어있다 가정했을때 : 1 1 + 1 2 >>
 	 * (1+1)+""+(1+2); result = 23 만약 더한 값이 지정 자리 수를 넘어갔을 경우(18자리) 그 값을 18자리로 맞춘다음
-	 * carry를 발생시켜 다음 자리 연산때 1을 더 합니다.
+	 * carry를 발생시켜 다음 자리 연산때 1을 더 //..///'[합니다.
 	 * 
 	 * @param one 들어오는 첫번째 피연산자(문자열)
 	 * @param two 들어오는 첫번째 피연산자(문자열)
@@ -37,36 +56,40 @@ public class CBigInteger {
 
 		subNumber(one, two);
 
-		boolean carry = false;
+		byte carry = 0;
 
 		int length = one.length() > two.length() ? two.length() / 18 + 1 : one.length() / 18 + 1;
-		long[] temp = one.length() > two.length() ? CBigInteger.one : CBigInteger.two;
+		long[] temp = one.length() > two.length() ? CBigIntegerOP.one : CBigIntegerOP.two;
 
-		final long compareDigit = 1000000000000000000L;
 		StringBuilder builder = new StringBuilder();
 
 		long t_value;
 
 		for (int i = 0; i < length; i++) {
-			t_value = CBigInteger.one[i] + CBigInteger.two[i] + (carry ? 1 : 0);
-			carry = false;
+			t_value = CBigIntegerOP.one[i] + CBigIntegerOP.two[i] + carry;
+			carry = 0;
 			if (t_value / compareDigit == 1) {
-				carry = true;
+				carry = 1;
 				String t_number = String.valueOf(t_value).substring(1);
 				builder.insert(0, t_number);
 				continue;
 			}
+			
+			if(t_value == 0) break;
+
 			builder.insert(0, t_value);
 		}
 
 		// 피연산자끼리 자리수가 안맞을경우 큰 자리수를 가진 피연산자의 남은 값을 넣습니다.
 		for (int i = length; i < temp.length; i++) {
-			t_value = temp[i] + (carry ? 1 : 0);
+			t_value = temp[i] + carry;
+			if(t_value == 0) break;
 			builder.insert(0, t_value);
 		}
 
-		return printExpression(one, "+", two, builder);
+		return builder.toString();
 	}
+	
 
 	/**
 	 * 
@@ -83,15 +106,16 @@ public class CBigInteger {
 		int oneLength = one.length() / 18 + 1;
 		int twoLength = two.length() / 18 + 1;
 		int index = 0;
-
-		CBigInteger.one = new long[oneLength];
-		CBigInteger.two = new long[twoLength];
-		for (index = one.length(); index >= 0 ; index -= 18) 
-			CBigInteger.one[(oneLength - 1) - index / 18] = Long
-					.parseLong(one.substring(index - 18 < 0 ? 0 : index-18, index));
 		
-		for (index = two.length(); index >= 0 ; index -= 18)
-			CBigInteger.two[(twoLength - 1) - index / 18] = Long
+		CBigIntegerOP.one = new long[oneLength];
+		CBigIntegerOP.two = new long[twoLength];
+		for (index = one.length(); index > 0 ; index -= 18) { 
+	
+			CBigIntegerOP.one[(oneLength - 1) - index / 18] = Long
+					.parseLong(one.substring(index - 18 < 0 ? 0 : index-18, index));
+		}
+		for (index = two.length(); index > 0 ; index -= 18)
+			CBigIntegerOP.two[(twoLength - 1) - index / 18] = Long
 					.parseLong(two.substring(index - 18 < 0 ? 0 : index-18, index));
 	}
 
@@ -218,9 +242,5 @@ public class CBigInteger {
 		return value.toString();
 	}
 
-	public static void main(String[] args) {
-		System.out.println(add("9999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999", "9999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999"));
-
-	}
 
 }
